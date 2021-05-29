08d971ac2fd80a86bc14841a6ed065c5768348ce
Botiha <wokiyi4490@geeky83.com>
2021-05-29 13:43:30 +0300

    Text

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path')
+const DB_PATH = path.join(__dirname,'winx','winx.json');
+
+const server = http.createServer((req, res)=>{
+if(req.url === '')
+}
+
+
+)
