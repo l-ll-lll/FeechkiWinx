2adbdd0ed27f7dc43373f0ce3f7ce289bd8687a3
RomaniukDenis <den.romaniuk.2010@gmail.com>
2021-06-05 13:13:51 +0300

    Nichego ne pomenyalos

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-var colorArray = ["#FF69B4", "#C71585", "#FFA07A", "#F0E68C", "#F4A460", "#6495ED"];
+var colorArray = ["#FF69B4", "#FFB6C1", "#FFA07A", "#F0E68C", "#F4A460", "#6495ED", "#008080"];
 var i = 0;
 
 function changeFone(){
