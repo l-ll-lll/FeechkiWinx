766a1d22691dedda58dc68c03a29c29a0d6db0ed
Sonya638 <solodovnyksophia4@gamil.com>
2021-06-12 13:17:03 +0300

    'fghjtf'

--- frontend/xhr.js
+++ frontend/xhr.js
@@ -4,7 +4,14 @@ let xhr = new XMLHttpRequest();
 xhr.open('GET', url);
 
 xhr.onload = () => {
-    document.getElementById('joke').innerHTML = xhr.response;
+    let jokes = JSON.parse(xhr.response);
+    jokes.map(joke => {
+            document.getElementById('joke').innerHTML += `
+    Joke:${joke.joke} Autor:${joke.autor}
+    `;
+    })
+
+   
 }
 
 xhr.send();
@@ -13,5 +20,9 @@ function massage(){
     let xml = new XMLHttpRequest();
     let mas = document.getElementById('in').value;
     xml.open('POST', url);
-    xml.send(mas);
+    let joke = {
+        joke: mas,
+        autor: 'yura'
+    }
+    xml.send();
 }
