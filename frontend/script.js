cd5766ee0fd4a40301bb33e5be2a6c78867b40fb
RomaniukDenis <den.romaniuk.2010@gmail.com>
2021-06-12 13:55:34 +0300

    djkhfku

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 //Сделать чтобы пользователь мог самы выбирать конкретный цвет (Не пририотетное задание)
-var colorArray = ["#708090", "#FFB6C1", "#FFA07A", "#F0E68C", "#F4A460", "#6495ED", "#008080"];
+var colorArray = ["#708090", "#FFB6C1", "#FFA07A", "#F0E68C", "#F4A460", "#008080"];
 var i = 0;
 
 function changeFone(){
@@ -9,3 +9,9 @@ function changeFone(){
         i = 0;
     }
 }
+function change_color(obj)
+{
+
+  obj.value && (document.body.style.backgroundColor = obj.value);
+
+  }
