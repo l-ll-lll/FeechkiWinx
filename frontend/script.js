a4064f865d753b78cfa0aebc8299bc68093b0e70
RomaniukDenis <den.romaniuk.2010@gmail.com>
2021-06-13 18:48:24 +0300

    siufh

--- frontend/script.js
+++ frontend/script.js
@@ -1,9 +1,15 @@
+let color = window.localStorage.getItem('color'); 
+if(color){
+  document.body.style.backgroundColor = color;
+}
+
 
 function change_color(obj)
 {
   obj.value && (document.body.style.backgroundColor = obj.value);
+  window.localStorage.setItem('color',obj.value);
 }
-
+$('#HSO').hide();
 $('#HS').click(()=>{
    $("#HSO").toggle();
 })
